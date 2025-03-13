9c9760ae07a0cb3dd42f354cd84a8c1a966a5e2b
Mustafizur Rahman Sumon <mustafizur.rahman@startsmartz.net>
2025-03-14 05:59:10 +0600

    sumon's task completed

--- olympic-bd-client/src/App.jsx
+++ olympic-bd-client/src/App.jsx
@@ -2,13 +2,17 @@ import "./App.css";
 import CarouselComponent from "./componants/CarouselComponent";
 import Footer from "./componants/Footer";
 import Navbar from "./componants/Navbar/Navbar";
-import Hero from "./componants/hero/Hero"
+import Hero from "./componants/hero/Hero";
+import DelightsSection from "./pages/Home/DelightsSection";
+import DesiredBrands from "./pages/Home/DesiredBrands";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <CarouselComponent />
+      <DesiredBrands />
+      <DelightsSection />
       <Footer />
     </>
   );
