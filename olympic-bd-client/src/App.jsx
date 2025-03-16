95de8ecaecf2b37046201b630310e02e90796bfb
didarBro <didar131299@gmail.com>
2025-03-16 20:46:41 +0600

    all pages done

--- olympic-bd-client/src/App.jsx
+++ olympic-bd-client/src/App.jsx
@@ -3,11 +3,18 @@ import CarouselComponent from "./componants/CarouselComponent";
 import Footer from "./componants/Footer";
 import Navbar from "./componants/Navbar/Navbar";
 import Hero from "./componants/hero/Hero"
-import Activity from "./componants/pages/Activity";
-import Careers from "./componants/pages/Careers";
-import FullReport from "./componants/pages/FullReport";
-import PressReleases from "./componants/pages/PressRealeases";
-import VideoGallery from "./componants/pages/VideoGallery";
+import Activity from "./pages/Activity";
+import Careers from "./pages/Careers";
+import CorporateGovernance from "./pages/CorporateGovernance";
+import Exports from "./pages/Exports";
+import FullReport from "./pages/FullReport";
+import Overview from "./pages/Overview";
+import PressReleases from "./pages/PressRealeases";
+import ShareStructure from "./pages/ShareStructure";
+import StrategyAndInnovation from "./pages/StrategyAndInnovation";
+import SustainabilityOverview from "./pages/SustainabilityOverview";
+import VideoGallery from "./pages/VideoGallery";
+
 function App() {
   return (
     <>
@@ -15,11 +22,7 @@ function App() {
       <Hero />
       <CarouselComponent />
       <Footer />
-      <PressReleases/>
-      <Careers/>
-      <VideoGallery/>
-      <Activity/>
-      <FullReport/>
+      <Exports/>
     </>
   );
 }
